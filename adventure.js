5679723d817e60cf6560ea6cf124846bee705b7f
MegIsFabulouslyAmazing <30560752+MegIsFabulouslyAmazing@users.noreply.github.com>
2017-08-02 15:57:12 -0400

    Map is set

--- adventure.js
+++ adventure.js
@@ -1,7 +1,9 @@
 var userInput = "";
 var command = "";
 var room = 0;
-var text = ["Programming", "Claflin", "Quad West", "Tower West", "DO", "Archway West", "Quad East", "Great Room", "Tower East", "CO", "Archway East", "Alumni", "KSC", "Severance", "Art Room", "Board Game Room", "Founders", "Lulu Wang", "Green", "Pendleton", "Dana Hall", "You can't go that way!"];
+var location = ["Programming", "Claflin", "Quad West", "Tower West", "DO", "Archway West", "Quad East", "Great Room", "Tower East", "CO", "Archway East", "Alumni", "KSC", "Severance", "Art Room", "Board Game Room", "Founders", "Lulu Wang", "Green", "Pendleton", "Dana Hall", "You can't go that way!"];
+
+var room = ["Welcome to the PO! This is where we make the magic happen."]
 
 function keys(event) {
     //record which key was pressed
@@ -38,40 +40,256 @@ function getCommand(){
         command = "none";
     }
     console.log(command);
+    console.log("room: ", room);
 }
 
 function changeRoom(){
     if (room == 0){
         switch(command){
             case "n":
-                addNewText(text[1]);
                 room = 1;
                 break;
             case "e":
-                addNewText(text[2]);
                 room = 2;
                 break;
             default:
-                addNewText(text[21]);
                 break;
         }
     }
     else if (room == 1){
         switch(command){
-            case "n":
-                addNewText(text[3]);
-                room = 3;
-                break;
             case "e":
-                addNewText(text[6]);
-                room = 6;
+                room = 2;
                 break;
-            case "w":
-                addNewText(text[0]);
+            case "s":
                 room = 0;
                 break;
             default:
-                addNewText(text[21]);
+                break;
+        }
+    }
+    else if (room == 2){
+        switch(command){
+            case "n":
+                room = 3;
+                break;
+            case "e":
+                room = 6;
+                break;
+            case "w":
+                room = 0;
+                break;
+            default:
+                break;
+        }
+    }
+    else if (room == 3){
+        switch(command){
+            case "n":
+                room = 4;
+                break;
+            case "s":
+                room = 2;
+                break;
+            default:
+                break;
+        }
+    }
+    else if (room == 4){
+        switch(command){
+            case "n":
+                room = 5;
+                break;
+            case "e":
+                room = 7;
+                break;
+            case "s":
+                room = 3;
+                break;
+            default:
+                break;
+        }
+    }
+    else if (room == 5){
+        switch(command){
+            case "e":
+                room = 10;
+                break;
+            case "s":
+                room = 4;
+                break;
+            default:
+                break;
+        }
+    }
+    else if (room == 6){
+        switch(command){
+            case "n":
+                room = 8;
+                break;
+            case "e":
+                room = 13;
+                break;
+            case "w":
+                room = 2;
+                break;
+            default:
+                break;
+        }
+    }
+    else if (room == 7){
+        switch(command){
+            case "e":
+                room = 9;
+                break;
+            case "w":
+                room = 4;
+                break;
+            default:
+                break;
+        }
+    }
+    else if (room == 8){
+        switch(command){
+            case "n":
+                room = 9;
+                break;
+            case "s":
+                room = 6;
+                break;
+            default:
+                break;
+        }
+    }
+    else if (room == 9){
+        switch(command){
+            case "n":
+                room = 10;
+                break;
+            case "s":
+                room = 8;
+                break;
+            case "w":
+                room = 7;
+                break;
+            default:
+                break;
+        }
+    }
+    else if (room == 10){
+        switch(command){
+            case "n":
+                room = 11;
+                break;
+            case "e":
+                room = 16;
+                break;
+            case "s":
+                room = 9;
+                break;
+            case "w":
+                room = 5;
+                break;
+            default:
+                break;
+        }
+    }
+    else if (room == 11){
+        switch(command){
+            case "n":
+                room = 12;
+                break;
+            case "e":
+                room = 17;
+                break;
+            case "s":
+                room = 10;
+                break;
+            default:
+                break;
+        }
+    }
+    else if (room == 12){
+        switch(command){
+            case "s":
+                room = 11;
+                break;
+            default:
+                break;
+        }
+    }
+    else if (room == 13){
+        switch(command){
+            case "n":
+                room = 14;
+                break;
+            case "w":
+                room = 6;
+                break;
+            default:
+                break;
+        }
+    }
+    else if (room == 14){
+        switch(command){
+            case "n":
+                room = 15;
+                break;
+            case "w":
+                room = 6;
+                break;
+            default:
+                break;
+        }
+    }
+    else if (room == 15){
+        switch(command){
+            case "w":
+                room = 6;
+                break;
+            default:
+                break;
+        }
+    }
+    else if (room == 16){
+        switch(command){
+            case "e":
+                room = 18;
+                break;
+            case "w":
+                room = 10;
+                break;
+            default:
+                break;
+        }
+    }
+    else if (room == 17){
+        switch(command){
+            case "w":
+                room = 11;
+                break;
+            default:
+                break;
+        }
+    }
+    else if (room == 18){
+        switch(command){
+            case "e":
+                room = 19;
+                break;
+            case "w":
+                room = 16;
+                break;
+            default:
+                break;
+        }
+    }
+    else if (room == 19){
+        switch(command){
+            case "w":
+                room = 18;
+                break;
+            default:
                 break;
         }
     }
@@ -86,7 +304,7 @@ function addNewText(){
     //create new paragraph
     var para = document.createElement("p");
     //create text node
-    var node = document.createTextNode(text[room]);
+    var node = document.createTextNode(location[room]);
     //put text node in new paragraph
     para.appendChild(node);
     //get the element you want to append the paragraph to
